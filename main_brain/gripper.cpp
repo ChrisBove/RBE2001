90791c3c614b3d343a2bd735cfadf1e574c35a88
cmellen <cmellen@wpi.edu>
2014-10-08 08:04:35 -0400

    um, some code
    not really sure what i did

--- main_brain/gripper.cpp
+++ main_brain/gripper.cpp
@@ -3,6 +3,9 @@
 
 #include <Servo.h>
 
+Gripper::Gripper(gripperState state ) {
+  thoughtState = state;
+}
 
 Gripper::Gripper(int gripServo, int rackServo) {
   _gripServo = gripServo;
@@ -12,23 +15,59 @@ Gripper::Gripper(int gripServo, int rackServo) {
 void Gripper::attachMotors() {
   grip.attach(_gripServo);
   rack.attach(_rackServo);
-  rack.write(90);
-  grip.write(45);
-}
-
-void Gripper::extend(){
   rack.write(25);
-}
-
-void Gripper::retract(){
-  rack.write(150);
-}
-
-void Gripper::openGrip(){
   grip.write(45);
 }
 
-void Gripper::closeGrip(){
-  grip.write(145);
+void Gripper::setReactTime() {
+  reactTime = millis();
 }
 
+bool workArm(int state) {
+  switch (state) {
+    case Gripper::INIT_CGRIP:
+      setReactTime();
+      break;
+    case Gripper::CGRIP:
+      bool result = closeGrip();
+      if (result) {
+        break;
+      }
+  }
+}
+
+bool Gripper::extend() {
+  int timeLapse = millis() - reactTime;
+  if (timeLapse <= 1000) {
+    rack.write(25);
+    return true;
+  }
+  return false;
+}
+
+bool Gripper::retract() {
+  int timeLapse = millis() - reactTime;
+  if (timeLapse <= 1000) {
+    rack.write(150);
+    return true;
+  }
+  return false;
+}
+
+bool Gripper::openGrip() {
+  int timeLapse = millis() - reactTime;
+  if (timeLapse <= 500) {
+    grip.write(45);
+    return false;
+  }
+  return true;
+}
+
+bool closeGrip() {
+  int timeLapse = millis() - reactTime;
+  if (timeLapse <= 500) {
+    grip.write(145);
+    return true;
+  }
+  return false;
+}
--- main_brain/gripper.h
+++ main_brain/gripper.h
@@ -7,18 +7,26 @@
 
 class Gripper {
   public:
+  enum gripperState {WAIT_FOR_INSTRUCT, INIT_CGRIP, CGRIP, INIT_RETRACT1, RETRACT, INIT_EXTEND, EXTEND, INIT_OGRIP1, OGRIP1, INIT_CGRIP2, CGRIP2, INIT_RETRACT2};
+  Gripper(gripperState state);
+    
+    gripperState thoughtState = INIT_CGRIP;
+    
     Gripper(int gripServo, int rackServo);
     void attachMotors();
-    void extend();
-    void retract();
-    void openGrip();
-    void closeGrip();
+    bool extend();
+    bool retract();
+    bool openGrip();
+//    bool closeGrip();
+    void setReactTime();
     
   private:
+  bool closeGrip();
     Servo rack;
     Servo grip;
     int _gripServo;
     int _rackServo;
+    int reactTime = 0;
 };
 
 #endif
