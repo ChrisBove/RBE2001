586b876a69a22c254be7bac7bb71bed93c9c7959
ChrisBove <bove.3123@gmail.com>
2014-10-11 15:14:27 -0400

    tweaked turns

--- main_brain/drive_train.cpp
+++ main_brain/drive_train.cpp
@@ -93,7 +93,7 @@ void DriveTrain::setTime() {
 // returns 1 when the turn is done
 bool DriveTrain::turn45(bool isRight) {
   int timeLapse = millis() - startTime;
-  if (timeLapse <= 600) {
+  if (timeLapse <= 550) {
     if (isRight)
       turn(110, 110);
     else
@@ -108,7 +108,7 @@ bool DriveTrain::turn45(bool isRight) {
 
 bool DriveTrain::turn180(bool isRight) {
   int timeLapse = millis() - startTime;
-  if (timeLapse <= 1250) {
+  if (timeLapse <= 1350) {
     if (isRight) {
       turn(110, 110);
     }
