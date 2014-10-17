2ca30cb4ae637e742001f152a7181f5693c5977d
ChrisBove <bove.3123@gmail.com>
2014-10-16 22:52:51 -0400

    commented vex controller

--- main_brain/vex_controller.cpp
+++ main_brain/vex_controller.cpp
@@ -1,22 +1,31 @@
+/**
+ ********************************************************************************************************
+ * @file    vex_controller.cpp
+ * @brief   vex_controller class methods
+ * @details handles controller related functions
+ ********************************************************************************************************
+ */
+/*** INCLUDE FILES ***/
+
 #include "Arduino.h"
 #include "vex_controller.h"
 
 #include "PPM.h"
 
-PPM ppm(2);
+PPM ppm(2); // create PPM object
 
 Controller::Controller(int pin) {
-  Serial.begin(115200);
+  Serial.begin(115200); // begin serial comms
 }
 
 int Controller::getControllerChannel(int channel) {
-  return ppm.getChannel(channel);
+  return ppm.getChannel(channel); // return the channelreading
 }
 
 bool Controller::isDownPressed(int channel) {
-  return ppm.getChannel(channel) > 130;
+  return ppm.getChannel(channel) > 130; // if channel is greater than some threshold, button is pressed down
 }
 
 bool Controller::isUpPressed(int channel) {
-  return ppm.getChannel(channel) < 50;
+  return ppm.getChannel(channel) < 50; // if channel is less than some threshold, button is pressed up
 }
--- main_brain/vex_controller.h
+++ main_brain/vex_controller.h
@@ -1,3 +1,11 @@
+/**
+ ********************************************************************************************************
+ * @file    vex_controller.h
+ * @brief   vex controller class
+ * @details handles controller methods
+ ********************************************************************************************************
+ */
+
 #ifndef vex_controller_h
 #define vex_controller_h
 
@@ -5,10 +13,31 @@
 
 class Controller {
   public:
+    /**
+   * @brief   Class constructor for controller
+   * @param   a pin... if we could choose which Serial to use in the future
+   * @return  Controller object
+   */
     Controller(int pin);
+   /**
+   * @brief   returns the selected channel value
+   * @param   channel number
+   * @return  int value from the channel (something around 0-180)
+   */
     int getControllerChannel(int channel);
+   /**
+   * @brief   checks if the up button is pressed on the passed channel
+   * @param   channel number to check buttons on
+   * @return  True if button is pressed, false if otherwise
+   */
     bool isUpPressed(int channel);
+   /**
+   * @brief   checks if the down button is pressed on the passed channel
+   * @param   channel number to check buttons on
+   * @return  True if button is pressed, false if otherwise
+   */
     bool isDownPressed(int channel);
+  
   private:
     
 };
