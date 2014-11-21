606899b1b07c94d4d804b309fc5706c814180328
ChrisBove <bove.3123@gmail.com>
2014-11-21 16:44:20 -0500

    implement encoder test 1

--- encoder_uno/encoder_uno.ino
+++ encoder_uno/encoder_uno.ino
@@ -4,12 +4,24 @@
   
 */
 
+#include <Encoder.h>
+
+Encoder left (2, 3);
+// TODO implement other encoder
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  
 }
+
+long oldPosition = -999;
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  long newPosition = left.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
 }
