f559660f46962d27dbc458845042ccd3dc88ae51
ChrisBove <bove.3123@gmail.com>
2014-11-21 18:45:18 -0500

    implementation of dead reckoning

--- encoder_uno/encoder_uno.ino
+++ encoder_uno/encoder_uno.ino
@@ -6,7 +6,7 @@
 
 #include <Encoder.h>
 
-Encoder left (2, 3);
+Encoder left (2, 4);
 Encoder right (3, 5);
 
 void setup() {
@@ -20,24 +20,51 @@ long r_oldPosition = -999;
 float theta = 0;
 float x = 0;
 float y = 0;
-#define radius 1.375;    // radius of drive wheels
-#define distance 9.63;    // separation between wheels
-#define ticksPerRot 3200;  // num encoder ticks for full rot.
+#define radius 1.375    // radius of drive wheels
+#define distance 9.63    // separation between wheels
+#define ticksPerRot 3200  // num encoder ticks for full rot.
+#define piDivTr  0.000981748  // PI/ticksPerRot
 
 void loop() {
   // put your main code here, to run repeatedly:
   long l_newPosition = left.read();
-  long r_newPosition = right.read();
+  long r_newPosition = -right.read();
+  bool update = false;
   if (l_newPosition != l_oldPosition) {
     l_oldPosition = l_newPosition;
+    update = true;
 //    Serial.println(l_newPosition);
   }
   if (r_newPosition != r_oldPosition) {
     r_oldPosition = r_newPosition;
+    update = true;
+//    Serial.print("right: ");
+//    Serial.println(r_newPosition);
   }
-  
-  
-  
+  update = false;
+  if (update) {
+    calcHeading();
+    calcPos();
+    Serial.print("Heading: ");
+    Serial.print(theta);
+    Serial.print("\t X: ");
+    Serial.print(x);
+    Serial.print("\t Y: ");
+    Serial.println(y);
+  }
 }
 
+void calcHeading() {
+  theta = 2*PI* ((float) radius/ (float) distance) * ( (float)(l_oldPosition - r_oldPosition)/ (float) ticksPerRot);
+}
 
+void calcPos() {
+  long sum = l_oldPosition + r_oldPosition;
+  x = radius * cos(theta) * sum * piDivTr;
+  y = radius *sin(theta) * sum * piDivTr;
+}
+
+void reset() {
+  l_oldPosition = 0;
+  r_oldPosition = 0;
+}
