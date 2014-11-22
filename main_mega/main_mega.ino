964bf512bb3af9fdb2534edbe90c30ca865d683c
ChrisBove <bove.3123@gmail.com>
2014-11-22 14:00:29 -0500

    creation of control for the arduino mega

---
+++ main_mega/main_mega.ino
@@ -0,0 +1,15 @@
+/*
+  This program runs on the Arduino Mega to control robot
+  functions.
+  
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
