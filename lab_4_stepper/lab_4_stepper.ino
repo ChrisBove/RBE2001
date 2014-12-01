0159e49e495601763ffc294f2ffe8bbd005f1434
ChrisBove <bove.3123@gmail.com>
2014-12-01 15:56:29 -0500

    update pins, add some switching

--- lab_4_stepper/lab_4_stepper.ino
+++ lab_4_stepper/lab_4_stepper.ino
@@ -2,10 +2,13 @@
 
 // steps a stepper motor about
 
-#define stepPin 10
-#define dirPin 11
+#define stepPin 11
+#define dirPin 10
 
+// use 10000 microseconds, or 10ms
+#define time 10000
 
+bool dir = false; // false for one dir
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,10 +18,22 @@ void setup() {
   // initialize to off
   digitalWrite(stepPin, LOW);
   digitalWrite(dirPin, LOW);
+  
+  Timer1.initialize(time);
+  Timer1.attachInterrupt( timerISR );
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+}
 
+/// --------------------------
+/// Custom ISR Timer Routine
+/// --------------------------
+void timerISR()
+{
+    // Toggle LED
+    digitalWrite( stepPin, digitalRead( stepPin ) ^ 1 );
 }
