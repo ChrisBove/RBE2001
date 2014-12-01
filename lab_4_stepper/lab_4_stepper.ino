4714eb17be3cd7fd7be10dc00532fe59d692e7f7
ChrisBove <bove.3123@gmail.com>
2014-12-01 15:34:11 -0500

    initialization of pins

--- lab_4_stepper/lab_4_stepper.ino
+++ lab_4_stepper/lab_4_stepper.ino
@@ -1,7 +1,22 @@
+#include <TimerOne.h>
+
+// steps a stepper motor about
+
+#define stepPin 10
+#define dirPin 11
+
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  
+  // initialize to off
+  digitalWrite(stepPin, LOW);
+  digitalWrite(dirPin, LOW);
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
