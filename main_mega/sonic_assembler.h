5e6cd2de0f1fa44a0ab2acbb9dfd3896df5a8dd4
ChrisBove <bove.3123@gmail.com>
2014-12-08 13:51:11 -0500

    more headers for methods

--- main_mega/sonic_assembler.h
+++ main_mega/sonic_assembler.h
@@ -13,10 +13,28 @@
 
 class SonicAssembler {
   public:
-    
+   /**
+   * @brief   Class constructor for sonic assembler
+   * @param   none
+   * @return  sonic assembler object
+   */
+   SonicAssembler();
+   /**
+   * @brief   Runs all service routines
+   * @param   None
+   * @return  None
+   */
+   void service();
+   /**
+   * @brief   Runs all service routines
+   * @param   angle in degrees (-90 to 90), int distance inches
+   * @return  None
+   */
+   void assemble(int angle, int distance);
+   
   private:
-    
-    
+   
+   
   
 };
 
