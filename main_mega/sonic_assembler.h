b3b23ee2751f2411a5306039a1681fe7b7559005
ChrisBove <bove.3123@gmail.com>
2014-12-08 13:16:50 -0500

    created sonic assembler class

---
+++ main_mega/sonic_assembler.h
@@ -0,0 +1,23 @@
+/**
+ ********************************************************************************************************
+ * @file    sonic_assembler.h
+ * @brief   sonic assembler class
+ * @details Assembles direction vectors (point cloud) from sensor mast
+ ********************************************************************************************************
+ */
+
+#ifndef sonic_assembler_h
+#define sonic_assembler_h
+
+#include "Arduino.h"
+
+class SonicAssembler {
+  public:
+    
+  private:
+    
+    
+  
+};
+
+#endif
