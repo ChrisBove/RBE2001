a4b43099ed3671f6ab14967823042cf5dec3b410
ChrisBove <bove.3123@gmail.com>
2014-12-08 21:17:07 -0500

    new navigator class

---
+++ main_mega/navigator.cpp
@@ -0,0 +1,18 @@
+/**
+ ********************************************************************************************************
+ * @file    navigator.h
+ * @brief   navigator class
+ * @details controls the robot using sensors and the drivetrain
+ ********************************************************************************************************
+ */
+ 
+/*** INCLUDE FILES ***/
+
+#include "Arduino.h"
+#include "navigator.h"
+
+Navigator::Navigator() {
+  
+}
+
+
