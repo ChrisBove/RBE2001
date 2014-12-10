f9cec2c430e3f403e444ab3fef219bb74be97a4f
SamiNeeno <smneeno@wpi.edu>
2014-12-10 16:59:16 -0500

    hacky fix
    correct navigator commit

--- main_mega/navigator.cpp
+++ main_mega/navigator.cpp
@@ -16,34 +16,56 @@
 #include "vfh.h"
 #include "cliff.h"
 
-
 // ************* CONSTANTS AND PINS ***************
-#define leftMotorPin    10
-#define rightMotorPin   11
-#define mastServoPin    9
-#define redFlamePin     0
-#define ultraPin        1
-#define digUltraPin     22
+#define LEFT_MOTOR_PIN    10
+#define RIGHT_MOTOR_PIN   11
+#define MAST_SERVO_PIN    9
+#define RED_FLAME_PIN     0
+#define ULTRA_PIN        1
+#define DIG_ULTRA_PIN     22
 #define LeftLight       10
 #define RightLight      11
 
+// globals:
+VFH::grid_t * myGrid;
+VFH::hist_t * myHist;
+
 // *************** instantiate class objects **************
-DriveTrain driveTrain(leftMotorPin, rightMotorPin, true, false); // left motor inverted, right not
-SensorMast sensorMast(mastServoPin, ultraPin, redFlamePin, digUltraPin);
+DriveTrain driveTrain(LEFT_MOTOR_PIN, RIGHT_MOTOR_PIN, true, false); // left motor inverted, right not
+SensorMast sensorMast(MAST_SERVO_PIN, ULTRA_PIN, RED_FLAME_PIN, DIG_ULTRA_PIN);
+SonicAssembler assembler;
 CliffDetector cliffDetect(RightLight, LeftLight);
+VFH vfh(); //&myGrid, &myHist);
+
+//myGrid = vfh.grid_init(50,10);
+//myHist = vfh.hist_init(2, 20, 10, 5);
+
 
 Navigator::Navigator() {
   // maybe stuff in some pointers to other objects that are passed.
 }
 
 void Navigator::setupNavigator() {
-  driveTrain.attachMotors(); // attach motors in drivetrain
+  driveTrain.setupDriveTrain(); // attach motors in drivetrain
   driveTrain.halt();         // stop the drivetrain motors
   sensorMast.setupMast();
 }
 
 void Navigator::service() {
-
+  driveTrain.service();
+  sensorMast.service();
+//  Serial.print("Grid update: ");
+//  Serial.print(vfh.grid_update(&myGrid, driveTrain.getX(), driveTrain.getY(), assembler.assemble(sensorMast.getServoAngle(), sensorMast.getDistance())) );
+////  Serial.print("\t Hist update: ");
+//  vfh.hist_update(&myHist, &myGrid);
+//  Serial.print("\t Dir: ");
+//  
+//  Serial.println(vfh.calculate_direction(&myHist, 90));
+  
+//  Serial.println(sensorMast.getServoAngle());
+//  delay(10);
+  driveTrain.halt();
+//  driveTrain.moveInDir(0);
 }
 
 
