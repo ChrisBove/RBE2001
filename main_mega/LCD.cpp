11099e10e77d1ac8242eb11b0c14d369d9c31a86
ChrisBove <bove.3123@gmail.com>
2014-12-10 16:33:26 -0500

    add lcd class

---
+++ main_mega/LCD.cpp
@@ -0,0 +1,15 @@
+/**
+ ********************************************************************************************************
+ * @file    lcd.cpp
+ * @brief   lcd class
+ * @details Controls LCD with certain parameters
+ ********************************************************************************************************
+ */
+/*** INCLUDE FILES ***/
+
+#include "Arduino.h"
+#include "sonic_assembler.h"
+
+LCD::LCD() {
+  // initialize
+}
---
+++ main_mega/LCD.h
@@ -0,0 +1,35 @@
+/**
+ ********************************************************************************************************
+ * @file    lcd.h
+ * @brief   lcd class
+ * @details Controls LCD with certain parameters
+ ********************************************************************************************************
+ */
+
+#ifndef lcd_h
+#define lcd_h
+
+#include "Arduino.h"
+
+class LCD {
+  public:
+   /**
+   * @brief   Class constructor for sonic assembler
+   * @param   none
+   * @return  sonic assembler object
+   */
+   LCD();
+   /**
+   * @brief   Runs all service routines
+   * @param   None
+   * @return  None
+   */
+   void service();
+   
+  private:
+   
+   
+  
+};
+
+#endif
