035d256bd356d49355088a0399a38b0ec2b1a0e4
SamiNeeno <smneeno@wpi.edu>
2014-12-13 16:21:02 -0500

    nuances to z-plane
    added function, proper led pins

--- main_mega/navigator.cpp
+++ main_mega/navigator.cpp
@@ -24,8 +24,8 @@
 #define RED_FLAME_PIN     0
 #define ULTRA_PIN        1
 #define DIG_ULTRA_PIN     22
-#define LED_indicator    12 
-#define LED_WIN          13 
+#define LED_indicator    25 
+#define LED_WIN          27 
 
 // *************** instantiate class objects **************
 DriveTrain driveTrain(LEFT_MOTOR_PIN, RIGHT_MOTOR_PIN, true, false); // left motor inverted, right not
@@ -109,17 +109,14 @@ void Navigator::chooseAction() {
 }
 
 void Navigator::candle_Position(){
-  float filler_z = 0.01;    //for the meantime 
-
   float x_coord = driveTrain.getX();
   float y_coord = driveTrain.getY();
   float head = driveTrain.getHeading(); 
-  float d = sensorMast.getDistance(); 
+  float d = sensorMast.getDistance()+6.5; 
   
   float aim; 
   float x_ref; 
   float y_ref; 
-  
 //now the x-y coordinates after the switch from "get close" to "extinguish" procedure
   const float pi = 3.14159;
   float x;
@@ -143,13 +140,12 @@ void Navigator::candle_Position(){
     y_ref = d*sin(aim);
   }
 //Find the z-coordinate: talk to chris E for the extraction function.
-  float d2 = cannon.getdistance(); 
-  float c_z = //height// + d2*sin(cannon.getangle());
+//  float c_z = 11.5 + d*tan(cannon.giveAngle());
 
 //-------------------------------------------------------------------
   float c_x = x_coord + x_ref;
   float c_y = y_coord + y_ref; 
-  float c_z = filler_z; 
+  float c_z = 0.00;
   my_lcd.updateLocation(c_x, c_y, c_z);
 }
 
